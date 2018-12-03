04f8d1a0673231a256cd1a04d6f9fec144e56dc6
wangzhou666 <wdx6666666@gmail.com>
2018-12-02 16:35:44 -0800

    move BaseRefresher to common.py

--- lib/common.py
+++ lib/common.py
@@ -1,3 +1,27 @@
 class UnrecongnizedDataException(Exception):
   pass
 
+
+class BaseRefresher(object):
+
+  def __init__(self, db_client):
+    self._client = db_client
+    self._collection = None
+    self._data = None
+
+  def _read(self):
+    pass
+
+  def _write(self):
+    if not self._collection:
+      raise RuntimeError('No MongoDB collection is specified for write destination.')
+    # TODO: write entity to mongodb
+    return
+
+  def refresh(self):
+    self._read()
+    self._write()
+
+  def __del__(self):
+    self._client.close()
+
--- lib/ticker.py
+++ lib/ticker.py
@@ -1,33 +1,10 @@
 import requests
 from bs4 import BeautifulSoup as bs
+from .common import BaseRefresher
 from .common import UnrecongnizedDataException
 
 
-class BaseIndexTickerRefresher(object):
-
-  def __init__(self, db_client):
-    self._client = db_client
-    self._collection = None
-    self._data = None
-
-  def _read(self):
-    pass
-
-  def _write(self):
-    if not self._collection:
-      raise RuntimeError('No MongoDB collection is specified for write destination.')
-    # TODO: write entity to mongodb
-    return
-
-  def refresh(self):
-    self._read()
-    self._write()
-
-  def __del__(self):
-    self._client.close()
-
-
-class SP500TickerRefresher(BaseIndexTickerRefresher):
+class SP500TickerRefresher(BaseRefresher):
   
   def __init__(self, db_client):
     super(SP500TickerRefresher, self).__init__(db_client)
